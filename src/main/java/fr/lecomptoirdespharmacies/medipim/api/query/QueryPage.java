565aa104f34d498cc2e15c98615066ba650b5164
Thomas <thomas@lecomptoirdespharmacies.fr>
2026-01-27 10:31:25 +0100

    LDS-5344: add check on QueryPage size

--- src/main/java/fr/lecomptoirdespharmacies/medipim/api/query/QueryPage.java
+++ src/main/java/fr/lecomptoirdespharmacies/medipim/api/query/QueryPage.java
@@ -1,3 +1,13 @@
 package fr.lecomptoirdespharmacies.medipim.api.query;
 
-public record QueryPage(int no, int size) {}
+import java.util.Set;
+
+public record QueryPage(int no, int size) {
+    private static final Set<Integer> VALID_SIZES = Set.of(10, 50, 100, 250);
+
+    public QueryPage {
+        if (!VALID_SIZES.contains(size)) {
+            throw new IllegalArgumentException("size must be one of: " + VALID_SIZES);
+        }
+    }
+}
